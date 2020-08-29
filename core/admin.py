bb0d98395d1f3e86ec96cd99356741695a648175
Nahian Ismail <nahiyan313@.gmailcom>
2020-08-29 14:49:32 +0600

    Update 29/8/2020

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Item, OrderItem, Order, Payment, Coupon, Refund
+from .models import Item, OrderItem, Order, Payment, Coupon, Refund, Address
 
 
 def make_refund_accepted(modeladmin, request, queryset):
@@ -12,13 +12,22 @@ make_refund_accepted.short_description = 'Update orders to refund granted'
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['user', 'ordered', 'being_delivered',
                     'received', 'refund_requested', 'refund_granted',
-                    'user', 'billing_address', 'payment', 'coupon']
-    list_display_links = ['user', 'billing_address', 'payment',
-                          'coupon']
+                    'user', 'billing_address', 'shipping_address',
+                    'payment', 'coupon']
+    list_display_links = ['user', 'billing_address', 'shipping_address',
+                          'payment', 'coupon']
     list_filter = ['user', 'ordered', 'being_delivered', 'received',
                    'refund_requested', 'refund_granted']
     search_fields = ['user__username', 'ref_code']
     actions = [make_refund_accepted]
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['user', 'street_address', 'apartment_address',
+                    'country', 'zip', 'address_type', 'default']
+    list_filter = ['default', 'address_type', 'country']
+    search_fields = ['user', 'street_address', 'apartment_address',
+                     'zip']
 
 
 admin.site.register(Item)
@@ -27,3 +36,4 @@ admin.site.register(Order, OrderAdmin)
 admin.site.register(Payment)
 admin.site.register(Coupon)
 admin.site.register(Refund)
+admin.site.register(Address, AddressAdmin)
--- core/models.py
+++ core/models.py
@@ -16,6 +16,20 @@ LABEL_CHOICES = (
     ('D', 'danger')
 )
 
+ADDRESS_CHOICES =(
+    ('B', 'Billing'),
+    ('S', 'Shipping')
+)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    stripe_customer_id = models.CharField(max_length=50, blank=True, null=True)
+    one_click_purchasing = models.BooleanField()
+
+    def __str__(self):
+        return self.user.username
+
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
@@ -75,13 +89,15 @@ class OrderItem(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
-    ref_code = models.CharField(max_length=50)
+    ref_code = models.CharField(max_length=50, blank=True, null=True)
     items = models.ManyToManyField(OrderItem)
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
     billing_address = models.ForeignKey(
-        'BillingAddress', on_delete=models.SET_NULL, blank=True, null=True)
+        'Address', related_name='billing_address', on_delete=models.SET_NULL, blank=True, null=True)
+    shipping_address = models.ForeignKey(
+        'Address', related_name='shipping_address', on_delete=models.SET_NULL, blank=True, null=True)
     payment = models.ForeignKey(
         'Payment', on_delete=models.SET_NULL, blank=True, null=True)
     coupon = models.ForeignKey(
@@ -107,16 +123,21 @@ class Order(models.Model):
         return final
 
 
-class BillingAddress(models.Model):
+class Address(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
     street_address = models.CharField(max_length=200)
     apartment_address = models.CharField(max_length=200)
     country = CountryField(multiple=False)
     zip = models.CharField(max_length=20)
+    address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES)
+    default = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
+
+    class Meta:
+        verbose_name_plural = 'Addresses'
 
 
 class Payment(models.Model):
@@ -146,3 +167,9 @@ class Refund(models.Model):
 
     def __str__(self):
         return f"{self.pk}"
+
+
+def userprofile_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        userprofile = UserProfile.objects.create(user=instance)
+        post_save.connect(userprofile_receiver, sender=settings.AUTH_USER_MODEL)
